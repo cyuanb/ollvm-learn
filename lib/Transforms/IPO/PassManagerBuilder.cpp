666ba1f34b641085f1b0a5e78fda68f552028570
Yuaii <2033810065@qq.com>
2025-03-22 21:28:48 +0800

    add ollvm

--- lib/Transforms/IPO/PassManagerBuilder.cpp
+++ lib/Transforms/IPO/PassManagerBuilder.cpp
@@ -48,6 +48,12 @@
 #include "llvm/Transforms/Vectorize/SLPVectorizer.h"
 #include "llvm/Transforms/EncodeFunctionName/EncodeFunctionName.h"
 
+#include "llvm/Transforms/Obfuscation/BogusControlFlow.h"
+#include "llvm/Transforms/Obfuscation/Flattening.h"
+#include "llvm/Transforms/Obfuscation/Split.h"
+#include "llvm/Transforms/Obfuscation/Substitution.h"
+#include "llvm/CryptoUtils.h"
+
 using namespace llvm;
 
 static cl::opt<bool>
@@ -152,6 +158,22 @@ static cl::opt<bool>
     EnableEncodeFunctionName("encode_function_name", cl::init(false), cl::Hidden,
                          cl::desc("Encode Function Name Pass"));
 
+// Flags for obfuscation
+static cl::opt<bool> Flattening("fla", cl::init(false),
+                                cl::desc("Enable the flattening pass"));
+
+static cl::opt<bool> BogusControlFlow("bcf", cl::init(false),
+                                      cl::desc("Enable bogus control flow"));
+
+static cl::opt<bool> Substitution("sub", cl::init(false),
+                                  cl::desc("Enable instruction substitutions"));
+
+static cl::opt<std::string> AesSeed("aesSeed", cl::init(""),
+                                    cl::desc("seed for the AES-CTR PRNG"));
+
+static cl::opt<bool> Split("split", cl::init(false),
+                           cl::desc("Enable basic block splitting"));
+
 PassManagerBuilder::PassManagerBuilder() {
     OptLevel = 2;
     SizeLevel = 0;
@@ -180,6 +202,13 @@ PassManagerBuilder::PassManagerBuilder() {
     PrepareForThinLTO = EnablePrepareForThinLTO;
     PerformThinLTO = EnablePerformThinLTO;
     DivergentTarget = false;
+
+    // Initialization of the global cryptographically
+    // secure pseudo-random generator
+    if(!AesSeed.empty()) {
+      if(!llvm::cryptoutils->prng_seed(AesSeed.c_str()))
+        exit(1);
+    }
 }
 
 PassManagerBuilder::~PassManagerBuilder() {
@@ -445,7 +474,10 @@ void PassManagerBuilder::populateModulePassManager(
 
     MPM.add(createEncodeFunctionName(EnableEncodeFunctionName));
 
-
+    MPM.add(createSplitBasicBlock(Split));
+    MPM.add(createBogus(BogusControlFlow));
+    MPM.add(createFlattening(Flattening));
+    MPM.add(createSubstitution(Substitution));
 
 
   // Allow forcing function attributes as a debugging and tuning aid.
